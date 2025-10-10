e5bd72d62b6edb7f67a37d45c08c32e65085ba5b
uni423 <unikid0423@gmail.com>
2025-10-11 00:06:55 +0900

    2025-10-11-LYJ-01 : 프로젝트 기본 구조 설계

---
+++ Assets/1_Scripts/Manager/GameManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public UserInfoData UserInfoData;
+
+    public GameStep gameStep;
+
+    protected void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(this.gameObject);
+
+        StartCoroutineMethod(TableBase.LoadAllDataTable());
+
+        Initialize();
+
+        SceneLoader.Load("MainScene");
+    }
+
+    public void Initialize()
+    {
+        UserInfoData = new UserInfoData();
+        UserInfoData.InitData();
+        UserInfoData.SaveData();
+    }
+
+#region Coroutine
+    public static Coroutine StartCoroutineMethod(IEnumerator enumerator)
+    {
+        return Instance.StartCoroutine(enumerator);
+    }
+
+    public static void StopCoroutineMethod(string name)
+    {
+        Instance.StopCoroutine(name);
+    }
+
+    public static void StopAllCoroutineMethod()
+    {
+        Instance.StopAllCoroutines();
+    }
+#endregion
+}
